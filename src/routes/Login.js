8f1bff5f8f3ad9f6d3d91d6a246436437a474dd1
MeanbyMin <soeg0810@gmail.com>
2021-05-31 17:03:34 +0900

    login.js

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,4 +1,24 @@
+import SocialLogin from "components/SocialLogin";
 import React from "react";
+import styled from "styled-components";
 
-const Login = () => <span>Login</span>;
+const AuthContainer = styled.div`
+  -webkit-flex-direction: column;
+  flex-direction: column;
+  height: 100vh;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-align-items: center;
+  align-items: center;
+  display: flex;
+  display: -webkit-flex;
+`;
+
+const Login = () => {
+  return (
+    <AuthContainer>
+      <SocialLogin />
+    </AuthContainer>
+  );
+};
 export default Login;
